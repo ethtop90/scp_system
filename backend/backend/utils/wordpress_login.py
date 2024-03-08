f9673dc11703cb78999de9c5e8b21e398e3d6bbb
successworld0405 <fullsuccess.world@gmail.com>
2024-03-09 01:22:03 +0900

    login_test_by_cookies

--- backend/backend/utils/wordpress_login.py
+++ backend/backend/utils/wordpress_login.py
@@ -1,4 +1,6 @@
-import requests
+import requests, time
+from selenium import webdriver
+
 
 url = 'https://ymgfg.co.jp/wp-login.php'
 
@@ -16,7 +18,18 @@ def wordpress_login(username, password):
     # Handle the response
     if response.status_code == 200:
         # Handle the successful response
-        print('cookies:', response.cookies)
+        cookies = response.cookies.get_dict()
+        
+        # Open Chrome browser and set cookies
+        driver = webdriver.Chrome()
+        driver.get("https://ymgfg.co.jp/wp-admin/")
+        for cookie in cookies:
+            driver.add_cookie({
+                'name': cookie,
+                'value': cookies[cookie]
+            })
+        driver.get("https://ymgfg.co.jp/wp-admin/")
+        time.sleep(20)
     else:
         print('Error:', response.status_code)
    
